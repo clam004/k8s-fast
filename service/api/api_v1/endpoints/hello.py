17835125d5ff6b3147d1e0852338e62d74ead47d
Ritom Gupta <ritomgupta99@gmail.com>
2023-05-15 16:18:44 -0700

    Add auth middleware, apikey dependency on route, cached Settings for api_key use

--- service/api/api_v1/endpoints/hello.py
+++ service/api/api_v1/endpoints/hello.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security.api_key import APIKey
 
 from service.core.models.output import MessageOutput
 from service.core.models.input import MessageInput
 from service.core.logic.business_logic import run_prime_factor_calculation
+import service.core.auth as auth
 
 router = APIRouter()
-
 
 @router.get("/example", tags=["example get"])
 def example_get():
@@ -21,8 +22,7 @@ def example_get():
 
 
 @router.post("/hello", response_model=MessageOutput, tags=["hello post"])
-def hello_endpoint(inputs: MessageInput):
-
+def hello_endpoint(inputs: MessageInput, api_key: APIKey = Depends(auth.get_api_key)):
     """
     Respond to requests on the hello endpoint
     """
@@ -30,8 +30,8 @@ def hello_endpoint(inputs: MessageInput):
     print(inputs)
 
     n, largest_prime_factor, elapsed_time = run_prime_factor_calculation()
-
     return {
+        "default variable": api_key,
         "message1": "Hello, world!",
         "message2": f"The largest prime factor of {n} is {largest_prime_factor}. Calculation took {elapsed_time:0.3f} seconds.",
         "n": n,
